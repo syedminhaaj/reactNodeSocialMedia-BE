a2b4cb2f4d2bfcb445bdea2793068e325e17b855
syed <syedminhaaj@gmail.com>
2024-10-15 15:47:46 -0400

    added google cloud db details

--- config/db.js
+++ config/db.js
@@ -1,12 +1,21 @@
 const mysql = require("mysql2");
 
 // Create a connection to the MySQL database
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root", // your MySQL username
+//   password: "minhaj123", // your MySQL password
+//   database: "reactNodeMedia", // the database you want to connect to
+// });
+
+// Below details are for google cloud server details
 const connection = mysql.createConnection({
-  host: "localhost",
-  post: 3306,
-  user: "root", // your MySQL username
-  password: "minhaj123", // your MySQL password
-  database: "reactNodeMedia", // the database you want to connect to
+  host: "34.171.203.150",
+  port: 3306,
+  user: "root",
+  password: "Test@123",
+  database: "reactnodeproject",
 });
 
 // Connect to the database
