8bce6b6711bf9847c3c1c0e820836343c68d8ee4
syed <syedminhaaj@gmail.com>
2025-01-18 11:53:29 -0500

    added firebase config file changes in render

--- config/firebase-db.js
+++ config/firebase-db.js
@@ -1,8 +1,8 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-config.js");
-
+const serviceAccount = require("./firebase-config");
+const firebaseConfig = JSON.parse(process.env.firebaseConfig);
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseConfig),
 });
 
 const db = admin.firestore();
--- config/firebase-tables.js
+++ config/firebase-tables.js
@@ -1,9 +1,10 @@
 // firebase-tables.js
 const admin = require("firebase-admin");
 const serviceAccount = require("./firebase-config");
+const firebaseConfig = JSON.parse(process.env.firebaseConfig);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseConfig),
 });
 
 const db = admin.firestore();
